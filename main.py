7a114be2a00c8466cecd5e646c30d6b5a3d8ad66
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2023-11-27 23:57:57 +0000

    Create api/random view

--- main.py
+++ main.py
@@ -15,5 +15,24 @@ def index():
             'date': datetime.now()}
 
 
+@app.route('/api/random')
+def random():
+    '''
+    Return to user a random number
+    '''
+    number_input = request.args.get('number', type=int, default=100)
+    text_input = request.args.get('text', type=str, default='default_text')
+
+    if isinstance(number_input, int):
+        return {
+            'input': number_input,
+            'random': randint(0, number_input),
+            'text': text_input,
+            'date': datetime.now()
+        }
+    else:
+        return {'Error': 'Please enter only numbers!'}
+
+
 if __name__ == '__main__':
     app.run()
